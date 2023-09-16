63ac6266894f09473f97e6a4eeb5f943362e1f73
Amin <amin@Amin.local>
2023-09-15 23:18:56 -0600

    HW1 answers

--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
@@ -10,7 +10,37 @@ std::string input(std::string prompt) {
 }
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    if (argc != 4) {
+        std::cout << "Usage: " << argv[0] << " <number1> <operation> <number2>" << std::endl;
+        return 1;
+    }
+
+    double num1 = std::strtod(argv[1], nullptr);
+    std::string operation = argv[2];
+    double num2 = std::strtod(argv[3], nullptr);
+
+    double result;
+
+    if (operation == "+") {
+        result = num1 + num2;
+    } else if (operation == "-") {
+        result = num1 - num2;
+    } else if (operation == "*") {
+        result = num1 * num2;
+    } else if (operation == "/") {
+        if (num2 != 0) {
+            result = num1 / num2;
+        } else {
+            std::cout << "Error: Division by zero is not allowed." << std::endl;
+            return 1;
+        }
+    } else {
+        std::cout << "Invalid operation. Please enter +, -, *, or /." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
+
--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/numbers_divisible_by.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/numbers_divisible_by.cpp
@@ -2,7 +2,28 @@
 #include <string>
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " start end divisor" << std::endl;
+        return 1;
+    }
+
+    int start = std::stoi(argv[1]);
+    int end = std::stoi(argv[2]);
+    int divisor = std::stoi(argv[3]);
+
+    if (start <= end) {
+        for (int i = start; i <= end; ++i) {
+            if (i % divisor == 0) {
+                std::cout << i << std::endl;
+            }
+        }
+    } else {
+        for (int i = start; i >= end; --i) {
+            if (i % divisor == 0) {
+                std::cout << i << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
